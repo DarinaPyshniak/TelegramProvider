c6de92cd39a2d9e87bfd2b7e10a8f175b83a6a46
Darina Pyshnyak <Darina Pyshnyak>
2017-05-04 12:56:09 +0300

    Load project to github

---
+++ TelegramProvider/MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Windows;
+
+namespace TelegramProvider {
+    public partial class MainWindow : Window {
+        private readonly SmsProviderTelegram _provider = new SmsProviderTelegram();
+        private readonly string _receiverPhoneNumder = "XXXXXXXXXXXX";
+        public MainWindow() {
+            InitializeComponent();
+            DataContext = this;
+        }
+
+        public string Message { get; set; }
+
+        private async void ButtonSendClick(object sender, RoutedEventArgs e) {
+            var isConnected = await _provider.Connect();
+            if (!isConnected)
+                return;
+            var id = await _provider.GetUserIdByPhoneNumber(_receiverPhoneNumder);
+            _provider.SendMessage(id, Message);
+            Message = string.Empty;
+        }
+    }
+}
---
+++ TelegramProvider/SmsProviderTelegram.cs
@@ -0,0 +1,50 @@
+﻿using System.Linq;
+using System.Threading.Tasks;
+using TeleSharp.TL;
+using TeleSharp.TL.Contacts;
+using TeleSharp.TL.Messages;
+using TLSharp.Core;
+
+namespace TelegramProvider {
+    public class SmsProviderTelegram {
+        private readonly string _apiHash = "********************************";
+        private readonly int _apiId = 11111;
+        private readonly TelegramClient _client;
+
+        public SmsProviderTelegram() {
+            _client = new TelegramClient(_apiId, _apiHash);
+        }
+
+        public async Task<bool> Connect() {
+            return await _client.ConnectAsync();
+        }
+
+        public async Task<int?> GetUserIdByPhoneNumber(string phoneNumber, string lastName = "", string firstName = "") {
+            var requestImportContacts = new TLRequestImportContacts {
+                contacts = new TLVector<TLInputPhoneContact>()
+            };
+            requestImportContacts.contacts.lists.Add(new TLInputPhoneContact {
+                phone = phoneNumber,
+                first_name = firstName,
+                last_name = lastName
+            });
+            var importedContact = await _client.SendRequestAsync<TLImportedContacts>(requestImportContacts);
+            return (importedContact.users.lists.First() as TLUser)?.id;
+        }
+
+        public async Task<int?> GetUserIdFromContactList(string phoneNumber) {
+            var contactList = await _client.GetContactsAsync();
+            var user = contactList.users.lists
+                                  .Where(x => x.GetType() == typeof (TLUser))
+                                  .Cast<TLUser>()
+                                  .FirstOrDefault(x => x.phone == phoneNumber);
+            var id = user?.id;
+            return id;
+        }
+
+        public async void SendMessage(int? userId, string message) {
+            if (userId != null)
+                await _client.SendMessageAsync(new TLInputPeerUser {user_id = (int) userId}, message);                
+        }
+    }
+}
